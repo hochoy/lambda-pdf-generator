1bd90ee7407f437004173dd7560c4a2bf7384445
Wai Ho Choy <choywaiho1@gmail.com>
2019-11-26 00:45:36 -0800

    added code to upload file to gdrive. simplified google service setup. added a few extra googledrive functions + links to other google drive functions

--- index.js
+++ index.js
@@ -21,44 +21,42 @@ const pool = new Pool({
   connectionTimeoutMillis: 5000, // max time for client to connect, else timeout
 });
 
-main().catch(console.error);
+// Define the main function
 
-// Main function
-async function main() {
+exports.handler = async function(event, context) {
 
   // query a database via pg-pool
   // using pool.query directly will release the connection once query returns/fails
   // using pool.connect().then(client => client.query()) will not release the connection until told to
-  pool
-  .query('SELECT * FROM customers;')
-  .then(res => console.log(`Database sample: ${JSON.stringify(res.rows, null, 2)}`))
-  .catch(e => {
-    console.error(e)
-    throw new Error(e)
-  })
+  const queryResult = await pool.query('SELECT * FROM customers;');
+  console.log(`Database query result:`,JSON.stringify(queryResult.rows,null,2));
+
 
   // connect to google drive and google sheet
-  const gKeyFile = path.join(__dirname, 'credentials', 'lambda-project-001-1a2f5773dd4f.json');
-  const gClient = await gAuthClient(gKeyFile);
-  const gDrive = await gDriveClient(gClient);
-  const gSheet = await gSheetClient(gClient);
+  // 1. a keyfile: 
+  // 2. a set of scope(s)/permission(s) = https://developers.google.com/identity/protocols/googlescopes
+  const googleKeyFile = path.join(__dirname, 'credentials', 'lambda-project-001-1a2f5773dd4f.json');
+  const googleAuth = new google.auth.GoogleAuth({ keyFile: googleKeyFile, scopes: [ 'https://www.googleapis.com/auth/drive' ] });
+  const googleClient = await googleAuth.getClient();
+  const googleDrive = await google.drive({ version: 'v3', auth: googleClient });
+  const googleSheet = await google.sheets({ version: 'v4', auth: googleClient });
 
-  // Get gdrive file list
-  const gDriveResponse = await gDrive.files.list();
-  const gDriveFiles = gDriveResponse.data;
-  console.log(`Gdrive files: ${JSON.stringify(gDriveFiles,null,2)}`);
+
+  // Get google drive file list
+  const googleDriveResponse = await googleDrive.files.list();
+  const googleDriveFiles = googleDriveResponse.data;
+  console.log(`Google drive files: ${JSON.stringify(googleDriveFiles,null,2)}`);
+
 
   // Get google sheet row data
-  const gSheetResponse = await gSheet.spreadsheets.values.batchGet({
+  const googleSheetResponse = await googleSheet.spreadsheets.values.batchGet({
     spreadsheetId: '1NJwphpY9zuzORHLYnk9Xh54KENvEGYlAUorJ1J8A4YU', // service user must have access to this spreadsheet
-    ranges: [
-      'Sheet1!A:E',
-      ]
+    ranges: ['Sheet1!A:E']
   });
-  console.log(`Gsheet raw response: ${JSON.stringify(gSheetResponse, null, 2)}`)
+  console.log(`Google sheet raw response: ${JSON.stringify(googleSheetResponse, null, 2)}`)
 
   // Reshape google sheet data as array of objects
-  const gSheetRawData = gSheetResponse.data.valueRanges[0].values;
+  const gSheetRawData = googleSheetResponse.data.valueRanges[0].values;
   const gSheetColNames = gSheetRawData[0].map(v => v.toLowerCase());
   const gSheetRows = gSheetRawData.slice(1);
 
@@ -72,8 +70,8 @@ async function main() {
     },{})
     return rowObject;
   })
-
   console.log(`Gsheet shaped data: ${JSON.stringify(gSheetShaped, null, 2)}`)
+
 
   // Write data to a template .odt file
   const odtData = {
@@ -84,59 +82,31 @@ async function main() {
   const templatePath = path.join(__dirname, 'templates', 'odt_template_1.odt');
   const odtPath = path.join('/tmp/', 'odt_w_data.odt');
   
-  await writeOdtFromTemplate(
+  const outputPath = await writeOdtFromTemplate(
     odtData,
     templatePath,
     odtPath,
   );
 
   //// BROKEN: Convert file using minified LibreOffice
-  //// NOTE: The current binary won't work on a mac
-  //// NOTE: The current binary might also not work on AWS Lambda due to changes in the image
+  //// NOTE: The current binary won't work on a mac as it was built on a Linux-based AMI for Lambda
+  //// NOTE: The current binary MIGHT also not work on AWS Lambda due to new updates in Lambda
   //// NOTE: The current binary needs to be re-hosted on my personal AWS S3/Github. The existing link points to an older S3 link.
-  //// SOLUTION: RE-COMPILING IN DESIRED ENVIRONMENT VIA DOCKER: https://github.com/vladgolubev/serverless-libreoffice/pull/22
+  //// SOLUTION 1: Test on AWS Lambda/Docker with similar environment since it was built for it
+  //// SOLUTION 2: RE-COMPILING IN DESIRED ENVIRONMENT VIA DOCKER: https://github.com/vladgolubev/serverless-libreoffice/pull/22
   // const libreOfficeExePath = await setupLibreOffice('https://s3.ca-central-1.amazonaws.com/davidchoy.libreoffice/lo.tar.gz','/tmp');
   // const pdfPath = await convertOdtToPdf(libreOfficeExePath, odtPath);  
-  
   // return pdfPath;
 
-  // Upload file to S3 or GoogleDrive, then cleanup
-}
+
+  // Upload file to GoogleDrive
+  // const uploadResult = await uploadToGoogle(googleDrive,outputPath,'application/pdf','1plA69VMEqNfdotYO5j4cKoHZlbIjmfsE');
+  const getResult = await getGoogleFile(googleDrive,'1fW-rvsHFoUs5R687BLMoItELvBA8KL0T');
+
+  // Upload file to S3 (requires AWS S3 permissions)
 
 
-
-async function gAuthClient(keyFile){
-  // Create a new JWT client using the key file downloaded from Google Developer Console
-  const auth = new google.auth.GoogleAuth({
-    keyFile: keyFile,
-    // full list of scopes
-    // https://developers.google.com/identity/protocols/googlescopes#drivev3
-    // https://developers.google.com/identity/protocols/googlescopes#sheetsv4
-    scopes: [
-      // 'https://www.googleapis.com/auth/spreadsheets', // drive permissions is a superset of spreadsheets
-      'https://www.googleapis.com/auth/drive', 
-    ]
-  });
-  const client = await auth.getClient();
-  return client;
-}
-
-async function gDriveClient(gClient){
-  // Obtain a new drive client
-  const drive = google.drive({
-    version: 'v3',
-    auth: gClient,
-  })
-  return drive;
-}
-
-async function gSheetClient(gClient){
-  // Obtain a new sheet client
-  const sheets = google.sheets({
-    version: 'v4',
-    auth: gClient,
-  });
-  return sheets;
+  return getResult;
 }
 
 async function writeOdtFromTemplate(data, templatePath, outputPath){
@@ -154,7 +124,7 @@ async function writeOdtFromTemplate(data, templatePath, outputPath){
       return(outputPath)
   }
   catch(err){
-      throw new Error(`ODT file creation failed: ${err.message}`)
+      throw new Error(`ODT file creation failed: ${err.stack}`)
   }
 }
 
@@ -180,7 +150,7 @@ if (!fs.existsSync(libreofficeExePath)){
         return libreofficeExePath;
     }
     catch(err){
-        throw new Error(`setupLibreOffice failed: Could not download libreoffice from ${s3Host}`);
+        throw new Error(`setupLibreOffice failed: Could not download libreoffice from ${s3Host}: ${err.stack}`);
     }
   } else {
       console.log('setupLibreOffice', 'Libreoffice already installed in this lambda instance');
@@ -216,7 +186,88 @@ async function convertOdtToPdf(libreofficeExePath, odtPath){
     return pdfPath;
   }
   catch(err) {
-    console.error(err.stack)
-    throw new Error(`convertOdtToPdf: ${err.message}`);
+    throw new Error(`convertOdtToPdf: ${err.stack}`);
   }
 }
+
+// official guide, easy: https://developers.google.com/drive/api/v3/quickstart/nodejs
+//https://github.com/googleapis/google-api-nodejs-client/tree/master/samples/drive
+
+async function uploadToGoogle(gDriveClient, filePath, fileType = 'application/pdf', googleFolderId) {
+
+  try {
+    const response = await gDriveClient.files.create({
+      requestBody: {
+        name: 'haha',
+        mimeType: 'application/pdf',
+        parents: [googleFolderId]
+      },
+      media: {
+        mimeType: fileType,
+        body: fs.createReadStream(filePath)
+      }
+    });
+
+    return response.data;
+  } catch (err) {
+    throw new Error(`uploadToGoogle: ${err.stack}`)
+  }
+}
+
+async function updateGoogleFile(gDriveClient, filePath, fileType = 'application/pdf', gFileId, newFileName) {
+  try {
+    const response = await gDriveClient.files.update({
+      fileId: gFileId,
+      // add metadata here
+      resource: {
+        name: newFileName ? newFileName : 'haha2'
+      },
+      media: {
+        mimeType: fileType,
+        body: fs.createReadStream(filePath)
+      }
+    });
+
+    return response.data;
+  } catch (err) {
+    throw new Error(`updateGoogleFile: ${err.stack}`);
+  }
+}
+
+async function getGoogleFile(gDriveClient, gFileId) {
+  try {
+    const response = await gDriveClient.files.get({
+      fileId: gFileId
+    });
+    
+    // EXAMPLE to write stream: https://github.com/googleapis/google-api-nodejs-client/blob/master/samples/drive/download.js
+    return response.data;
+  } catch (err) {
+    throw new Error(`getGoogleFile: ${err.stack}`);
+  }
+}
+
+async function listGoogleFiles(gDriveClient) {
+  // https://github.com/googleapis/google-api-nodejs-client/blob/master/samples/drive/quickstart.js
+  // https://github.com/googleapis/google-api-nodejs-client/blob/master/samples/drive/list.js
+}
+
+async function exportGoogleFile(gDriveClient, outputType, outputPath) {
+  // drive.files.export: https://github.com/googleapis/google-api-nodejs-client/issues/963#issuecomment-367671749
+  // as per issue, these are the types of data format that can be returned:
+  // googledrive on nodejs uses axios (an awesome http client) with these data-return options:
+  // 'arraybuffer', 'document', 'json', 'text', 'stream', browser only: 'blob'
+  // https://github.com/axios/axios#request-config
+}
+
+
+//// Call the main handler function. This is what AWS Lambda calls + the imports above the handler() definition
+exports.handler()
+  .then(function(result){
+    console.log(result);
+    // process.exit();
+  })
+  .catch(function(error) {
+    console.error(error);
+    process.exit();
+  });
